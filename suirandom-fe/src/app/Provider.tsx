5642c134dff9a24b0ff38d9b6d036c0e554fc423
Goraniya Karan <karangoraniya@gmail.com>
2025-02-10 23:43:20 +0530

    feat: dappkit provider added 🚀

---
+++ suirandom-fe/src/app/Provider.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import {
+  createNetworkConfig,
+  SuiClientProvider,
+  WalletProvider,
+} from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@mysten/dapp-kit/dist/index.css";
+
+const { networkConfig } = createNetworkConfig({
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+const queryClient = new QueryClient();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function Providers({ children }: any) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider>{children}</WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
